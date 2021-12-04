762c90218da535c5f7550711fc5101414e4570d4
Taniguchi <kv.taniguchi@icloud.com>
2021-12-03 22:08:42 -0600

    Initial Commit

---
+++ KVMarketSquare/KVMarketSquareApp.swift
@@ -0,0 +1,17 @@
+//
+//  KVMarketSquareApp.swift
+//  KVMarketSquare
+//
+//  Created by Kevin Taniguchi on 12/3/21.
+//
+
+import SwiftUI
+
+@main
+struct KVMarketSquareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
