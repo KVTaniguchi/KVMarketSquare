067f761b3d836736360824f411b13ff67ebeaae6
Kevin Taniguchi <ktaniguchi@squareup.com>
2021-12-16 16:06:12 -0600

    resolve conflicts

--- KVMarketSquare/Scenes/SearchView.swift
+++ KVMarketSquare/Scenes/SearchView.swift
@@ -97,34 +97,105 @@ struct SellerSearchResultsView: View {
     }
 }
 
+enum SearchCategoryFilters: String, CaseIterable {
+    case all = "All Sellers"
+    case food = "Food & Drink"
+    case retail = "Retail"
+    case restaurants = "Restaurants"
+    case coffeeAndTea = "Coffee & Tea"
+    case bakery = "Bakery"
+    case barClubLounge = "Bar or Club"
+    case beautyAndBarber = "Beauty or Barber"
+    case medical = "Medical"
+    case artsAndCrafts = "Arts & Crafts"
+    
+    func button(fetcher: SellerMultiSearchFetcher, selectedFilter: Binding<SearchCategoryFilters>) -> some View {
+        let isSelected = self == selectedFilter.wrappedValue
+        
+        return Button {
+            selectedFilter.wrappedValue = self
+            fetcher.filterResults(with: self)
+        } label: {
+            Text(self.rawValue).padding(4).border(isSelected ? .primary : .secondary, width: isSelected ? 4 : 1)
+        }
+        .contentShape(Rectangle())
+    }
+    
+    static func availableCases(for results: [SellerSearchResultViewModel]) -> [SearchCategoryFilters] {
+        var filters: [SearchCategoryFilters] = [.all]
+        
+        if results.contains(where: {
+            $0.sellerType == .foodAndDrink ||
+            $0.businessType == "food_truck_cart" ||
+            $0.businessType == "grocery_market" ||
+            $0.businessType == "food_stores_convenience_stores_and_specialty_markets" ||
+            $0.businessType == "restaurant" ||
+            $0.businessType == "coffee_tea_shop" ||
+            $0.businessType == "bakery"
+        }) {
+            filters.append(.food)
+        }
+        
+        if results.contains(where: { $0.sellerType == .retail || $0.businessType == "clothing_and_accessories" }) {
+            filters.append(.retail)
+        }
+        
+        if results.contains(where: {
+            $0.businessType == "restaurant"
+        }) {
+            filters.append(.restaurants)
+        }
+        
+        if results.contains(where: {
+            $0.businessType == "coffee_tea_shop"
+        }) {
+            filters.append(.coffeeAndTea)
+        }
+        
+        if results.contains(where: {
+            $0.businessType == "bar_club_lounge"
+        }) {
+            filters.append(.barClubLounge)
+        }
+
+        if results.contains(where: {
+            $0.businessType == "bakery"
+        }) {
+            filters.append(.bakery)
+        }
+        
+        if results.contains(where: {
+            $0.businessType == "beauty_and_barber_shops"
+        }) {
+            filters.append(.beautyAndBarber)
+        }
+        
+        if results.contains(where: {
+            $0.businessType == "medical_services_and_health_practitioners"
+        }) {
+            filters.append(.medical)
+        }
+        
+        if results.contains(where: {
+            $0.businessType == "artists_supply_and_craft_shops"
+        }) {
+            filters.append(.artsAndCrafts)
+        }
+        
+        return filters
+    }
+}
+
 struct SellerResultsListView: View {
-    @StateObject var task: FetchTask<SellerFromCoordsResult> = FetchTask<SellerFromCoordsResult>()
     @EnvironmentObject private var appData: AppData
     @State private var selectedStore: SellerAppData?
+    @State private var selectedFilter: SearchCategoryFilters = .all
     
     @StateObject var multiSearch: SellerMultiSearchFetcher = SellerMultiSearchFetcher()
     
-    private let url: URL
     private let coordinate: CLLocationCoordinate2D
     
     init?(coordinate : CLLocationCoordinate2D) {
-        // todo implement paging
-        //    'https://www.weebly.com/app/store/api/v1/seller-map/lat/{lat}/lng/{lng}?page=0&per_page=100'
-        var sellerMapComponents = URLComponents()
-            sellerMapComponents.scheme = "https"
-            sellerMapComponents.host = "weebly.com"
-            sellerMapComponents.path = "/app/store/api/v1/seller-map/lat/\(coordinate.latitude)/lng/\(coordinate.longitude)"
-            sellerMapComponents.queryItems = [
-                URLQueryItem(name: "page", value: "\(0)"), // not gonna care about this now
-                URLQueryItem(name: "per_page", value: "\(120)")
-            ]
-        
-        guard let url = sellerMapComponents.url else {
-            assertionFailure("no url")
-            return nil
-        }
-        
-        self.url = url
         self.coordinate = coordinate
     }
     
@@ -132,13 +203,21 @@ struct SellerResultsListView: View {
         Group {
             switch multiSearch.results {
             case .success(let models):
-                List(models) { model in
-                    Button {
-                        selectedStore = SellerAppData(store: model)
-                    } label: {
-                        FavoriteTileView(store: SellerAppData(store: model))
-                        // todo implement paging by inserting a view somewhere in list whose appearance triggers another page
-                        // track how many pages we have done
+                VStack {
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(spacing: 4) {
+                            ForEach(SearchCategoryFilters.availableCases(for: multiSearch.allModels), id: \.self) { filterType in
+                                filterType.button(fetcher: multiSearch, selectedFilter: $selectedFilter)
+                            }
+                        }
+                    }
+                    
+                    List(models) { model in
+                        Button {
+                            selectedStore = SellerAppData(store: model)
+                        } label: {
+                            FavoriteTileView(store: SellerAppData(store: model))
+                        }
                     }
                 }
             case .failure(let error):
@@ -147,7 +226,6 @@ struct SellerResultsListView: View {
                 ProgressView().onAppear(perform: {
                     withAnimation {
                         self.multiSearch.search(coordinate: self.coordinate)
-
                     }
                 })
             }
